1f3234aa7aeaafb5f99bbe52ac659ca93081a131
Kamlesh Bhatt <89186816+Kamlesh-Bhatt-52625@users.noreply.github.com>
2023-12-01 20:26:23 +0530

    Update Backdrop.jsx

--- client/src/canvas/Backdrop.jsx
+++ client/src/canvas/Backdrop.jsx
@@ -20,7 +20,7 @@ const Backdrop = () => {
         radius={9}
         intensity={0.55}
         ambient={0.25}
-        position={[5, 5, -10]}
+        position={[5, 5, -11]}
       />
       <RandomizedLight
         amount={4}
